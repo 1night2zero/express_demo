14f18612631903d2d1a2304501bfe5ce0961440b
HeGuican <guican.he@brother-bsh.com.cn>
2023-04-13 15:25:09 +0800

    init

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    // roots: [
+    //     "." // 测试目录
+    // ],
+    transform: {
+        "^.+\\.tsx?$": "ts-jest" // 匹配 .ts 或者 .tsx 结尾的文件
+    },
+    collectCoverage: true, // 统计覆盖率
+    testEnvironment: "node", // 测试环境ssss
+    // 不算入覆盖率的文件夹
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "/test/",
+        "/deps/",
+        "/build/"
+    ]
+    // collectCoverageFrom: [
+    //     "src/*.{ts,js}",
+    //     "src/**/*.{ts,js}"
+    // ],
+
+    // automock: false,
+    // clearMocks: true,
+    // collectCoverage: true,
+    // coverageDirectory: "coverage",
+    // coverageProvider: "v8",
+}
